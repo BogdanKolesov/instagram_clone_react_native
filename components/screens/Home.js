daba686720529414ae84177d3262e218badad1ba
Bogdan <bogdan_kolesov@bk.ru>
2022-04-12 19:59:11 +0600

    add: Home

--- components/screens/Home.js
+++ components/screens/Home.js
@@ -1,10 +1,37 @@
 import React from 'react';
-import { View, StyleSheet, Text } from 'react-native';
+import { View, StyleSheet, Text, StatusBar } from 'react-native';
+import FontAwesome from 'react-native-vector-icons/FontAwesome'
+import Feather from 'react-native-vector-icons/Feather'
+
 
 const Home = () => {
     return (
-        <View>
-            <Text>Home</Text>
+        <View style={{
+            backgroundColor: 'white',
+            height: '100%',
+        }}>
+            <StatusBar backgroundColor='white' barStyle='dark-content' animated={true} />
+            <View style={{
+                flexDirection: 'row',
+                justifyContent: 'space-between',
+                paddingHorizontal: 15,
+                alignItems: 'center'
+            }}>
+                <FontAwesome name='plus-square-o'
+                    style={{
+                        fontSize: 26
+                    }}
+                />
+                <Text style={{
+                    fontFamily: 'Lobster-Regular',
+                    fontSize: 26,
+                    fontWeight: '500'
+                }}>Kazgram</Text>
+                <Feather name='navigation' style={{
+                    fontSize: 26
+                }}
+                />
+            </View>
         </View>
     );
 }
