766f306b72c9d0f763096c9ce44387cb52539885
Bogdan <bogdan_kolesov@bk.ru>
2022-04-15 18:29:39 +0600

    fix: Search

--- components/screens/Search.js
+++ components/screens/Search.js
@@ -1,9 +1,13 @@
-import React from 'react';
-import { View, StyleSheet, Text, ScrollView, } from 'react-native';
+import React, { useState } from 'react';
+import { View, StyleSheet, Text, ScrollView, TouchableOpacity } from 'react-native';
 import SearchBox from '../screenComponents/SearchBox';
 import SearchContent from '../screenComponents/SearchContent';
+import AntDesign from 'react-native-vector-icons/AntDesign'
 
 const Search = () => {
+
+    const [image, setImage] = useState();
+
     return (
         <View style={{
             width: '100%',
@@ -14,6 +18,16 @@ const Search = () => {
             <ScrollView>
                 <SearchBox />
                 <SearchContent />
+                <TouchableOpacity style={{
+                    margin: 25,
+                    justifyContent: 'center',
+                    alignItems: 'center'
+                }}>
+                    <AntDesign name='pluscircleo' style={{
+                        fontSize: 48,
+                        opacity: 0.5
+                    }} />
+                </TouchableOpacity>
             </ScrollView>
         </View>
     );
