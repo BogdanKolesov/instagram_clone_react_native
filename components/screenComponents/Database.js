10f6ad828ecf63fa807bb83b1ddb08873e3e4362
Bogdan <bogdan_kolesov@bk.ru>
2022-04-16 19:33:35 +0600

    fix: ReelsSingle

--- components/screenComponents/Database.js
+++ components/screenComponents/Database.js
@@ -5,20 +5,25 @@ export const videoData = [
         title: 'Andrew_Wolf',
         description: 'From Armenia with love!',
         likes: '921',
-        isLike: false
+        isLike: false,
+        postProfile: require('../../storage/images/1.jpg')
     },
     {
         video: require('../../storage/videos/2.mp4'),
         title: 'Sofi_Lesova',
         description: 'Pretty.',
         likes: '311',
-        isLike: false
+        isLike: false,
+        postProfile: require('../../storage/images/3.jpg')
+
     },
     {
         video: require('../../storage/videos/3.mp4'),
         title: 'Alexander_Kameo',
         description: 'Peace, love, emphaty...',
         likes: '2',
-        isLike: false
+        isLike: false,
+        postProfile: require('../../storage/images/5.jpg')
+
     },
 ]
--- components/screenComponents/SingleReel.js
+++ components/screenComponents/SingleReel.js
@@ -1,5 +1,5 @@
 import React, { useRef, useState } from 'react';
-import { View, Text, StyleSheet, Dimensions, TouchableOpacity } from 'react-native';
+import { View, Text, StyleSheet, Dimensions, TouchableOpacity, Image } from 'react-native';
 import { Video } from 'expo-av';
 import Ionic from 'react-native-vector-icons/Ionicons'
 
@@ -55,11 +55,78 @@ const SingleReel = ({ item, index, currentIndex }) => {
                     position: 'absolute',
                     bottom: 300,
                     right: 30,
-                    backgroundColor: 'rgba(52,52,52,0.9)',
-                    padding: 10,
+                    backgroundColor: 'rgba(52,52,52,0.6)',
+                    padding: 12,
                     borderRadius: 100
-
                 }} />
+            <View style={{
+                position: 'absolute',
+                width: windowWidth,
+                zIndex: 1,
+                bottom: 80,
+                padding: 10
+            }}>
+                <View style={{
+
+                }}>
+                    <TouchableOpacity style={{
+                        width: 150
+                    }} >
+                        <View style={{
+                            width: '100%',
+                            flexDirection: 'row',
+                            alignItems: 'center'
+                        }}>
+                            <View style={{
+                                width: 60,
+                                height: 60,
+                                borderRadius: 100,
+                                backgroundColor: 'white',
+                                margin: 12
+                            }}>
+                                <Image source={item.postProfile}
+                                    style={{
+                                        position: 'absolute',
+                                        width: '100%',
+                                        height: '100%',
+                                        resizeMode: 'cover',
+                                        borderRadius: 100
+                                    }} />
+                            </View>
+                            <Text style={{
+                                fontSize: 20,
+                                color: 'white',
+                                fontWeight: 'bold'
+
+                            }}>
+                                {item.title}
+                            </Text>
+                        </View>
+                    </TouchableOpacity>
+                    <Text style={{
+                        color: 'white',
+                        fontSize: 14,
+                        marginHorizontal: 12
+                    }}>
+                        {item.description}
+                    </Text>
+                    <View style={{
+                        flexDirection: 'row',
+                        padding: 10
+                    }}>
+                        <Ionic name='ios-musical-note' style={{
+                            color: 'white',
+                            fontSize: 16
+                        }} />
+                        <Text style={{
+                            color: 'white',
+                            fontSize: 14
+                        }}>
+                            Original audio
+                        </Text>
+                    </View>
+                </View>
+            </View>
         </View>
     );
 }
