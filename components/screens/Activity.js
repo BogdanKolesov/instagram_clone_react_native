3548bf62434933342b59a2831c7ad78dd496efcf
Bogdan <bogdan_kolesov@bk.ru>
2022-04-17 10:18:28 +0600

    add: Activity

--- components/screens/Activity.js
+++ components/screens/Activity.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { View, StyleSheet, Text, ScrollView, TouchableOpacity, Image } from 'react-native';
+import { friendsProfileData } from '../screenComponents/Database';
 
 const Activity = () => {
     return (
@@ -24,6 +25,73 @@ const Activity = () => {
                 }}>
                     This week
                 </Text>
+                <View style={{
+                    flexDirection: 'row',
+                    paddingVertical: 10
+                }}>
+                    {
+                        friendsProfileData.slice(0, 3).map((data, index) => {
+                            return (
+                                <TouchableOpacity key={index}>
+                                    <Text>
+                                        {data.name}
+                                    </Text>
+                                </TouchableOpacity>
+                            )
+                        })
+                    }
+                    <Text>
+                        Started following you
+                    </Text>
+                </View>
+                <Text style={{
+                    fontWeight: 'bold',
+                    fontSize: 16
+                }}>
+                    Earlier
+                </Text>
+                {
+                    friendsProfileData.slice(3, 6).map((data, index) => {
+                        return (
+                            <View key={index} style={{
+                                width: '100%',
+
+                            }}>
+                                <View style={{
+                                    flexDirection: 'row',
+                                    justifyContent: 'space-between',
+                                    alignItems: 'center',
+                                    paddingVertical: 20,
+                                    width: '100%'
+                                }}>
+                                    <TouchableOpacity style={{
+                                        flexDirection: 'row',
+                                        justifyContent: 'space-between',
+                                        maxWidth: '64%',
+                                        alignItems: 'center'
+                                    }}>
+                                        <Image source={data.profileImage} style={{
+                                            width: 45,
+                                            height: 45,
+                                            borderRadius: 100,
+                                            marginRight: 10
+                                        }} />
+                                        <Text style={{
+                                            fontSize: 15
+                                        }}>
+                                            <Text style={{
+                                                fontWeight: 'bold'
+                                            }}>
+                                                {data.name}
+                                            </Text>
+                                            , who you might know, is on Bogdanogram
+                                        </Text>
+                                    </TouchableOpacity>
+                                </View>
+                            </View>
+                        )
+                    })
+                }
             </ScrollView>
         </View>
     );
