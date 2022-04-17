50454b172dbc7ddd6d88366d4a2bebc84fe035a9
Bogdan <bogdan_kolesov@bk.ru>
2022-04-17 10:05:02 +0600

    fix: database

--- components/screenComponents/Database.js
+++ components/screenComponents/Database.js
@@ -26,4 +26,101 @@ export const videoData = [
         postProfile: require('../../storage/images/5.jpg')
 
     },
+];
+
+
+
+//Friends profile Data
+
+export const friendsProfileData = [
+    {
+        name: 'Andrew_Wolf',
+        accoundName: 'Andrew Wolf',
+        profileImage: require('../../storage/images/3.jpg'),
+        posts: 176,
+        followwers: 987,
+        following: 123,
+        follow: false
+    },
+    {
+        name: 'Sofi_Lesova',
+        accoundName: 'Sofi Lesova',
+        profileImage: require('../../storage/images/4.jpg'),
+        posts: 426,
+        followwers: 941,
+        following: 113,
+        follow: false
+    },
+    {
+        name: 'Alexander_Kameo',
+        accoundName: 'Alexander Kameo',
+        profileImage: require('../../storage/images/1.jpg'),
+        posts: 2,
+        followwers: 41,
+        following: 7,
+        follow: false
+    },
+    {
+        name: 'Navalny',
+        accoundName: 'Alexey_Navalny',
+        profileImage: require('../../storage/images/6.jpg'),
+        posts: 999,
+        followwers: 947,
+        following: 14,
+        follow: false
+    },
+    {
+        name: 'Max_Katz',
+        accoundName: 'Maxim Katz',
+        profileImage: require('../../storage/images/5.jpg'),
+        posts: 9995866,
+        followwers: 996796,
+        following: 5436,
+        follow: false
+    },
+    {
+        name: 'Alexander_Lesov',
+        accoundName: 'Alexander Lesov',
+        profileImage: require('../../storage/images/3.jpg'),
+        posts: 11,
+        followwers: 94,
+        following: 11,
+        follow: false
+    },
+    {
+        name: 'Vadim_Smolianinov',
+        accoundName: 'Vadim Smolianinov',
+        profileImage: require('../../storage/images/5.jpg'),
+        posts: 1256,
+        followwers: 537,
+        following: 15,
+        follow: false
+    },
+    {
+        name: 'Daniel_K',
+        accoundName: 'Daniel K',
+        profileImage: require('../../storage/images/6.jpg'),
+        posts: 4,
+        followwers: 24,
+        following: 5,
+        follow: false
+    },
+    {
+        name: 'Alexander_Kameo',
+        accoundName: 'Alexander_Kameo',
+        profileImage: require('../../storage/images/4.jpg'),
+        posts: 14,
+        followwers: 27,
+        following: 16,
+        follow: false
+    },
+    {
+        name: 'Some_Guy',
+        accoundName: 'Some Guy',
+        profileImage: require('../../storage/images/2.jpg'),
+        posts: 3,
+        followwers: 2,
+        following: 4,
+        follow: false
+    },
 ]
--- components/screens/Activity.js
+++ components/screens/Activity.js
@@ -1,10 +1,30 @@
 import React from 'react';
-import { View, StyleSheet } from 'react-native';
+import { View, StyleSheet, Text, ScrollView, TouchableOpacity, Image } from 'react-native';
 
 const Activity = () => {
     return (
-        <View>
-
+        <View style={{
+            width: '100%',
+            height: '100%',
+            backgroundColor: 'white'
+        }} >
+            <Text style={{
+                fontSize: 20,
+                fontWeight: 'bold',
+                borderBottomWidth: 0.5,
+                borderBottomColor: '#dedede',
+                padding: 10
+            }}>Activity</Text>
+            <ScrollView style={{
+                margin: 10
+            }}>
+                <Text style={{
+                    fontSize: 16,
+                    fontWeight: 'bold'
+                }}>
+                    This week
+                </Text>
+            </ScrollView>
         </View>
     );
 }
