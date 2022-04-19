a3d7b79388c2f6b8afd1bf9dbb11f371c441dadd
Bogdan <bogdan_kolesov@bk.ru>
2022-04-19 15:50:04 +0600

    fix: ProfileBody

--- components/screenComponents/ProfileBody.js
+++ components/screenComponents/ProfileBody.js
@@ -20,78 +20,79 @@ export const ProfileBody = ({
 
                     </View>
                 ) : (
-                    <View style={{
-                        flexDirection: 'row',
-                        alignItems: 'center',
-                        justifyContent: 'space-around',
-                        paddingVertical: 20
-                    }}>
-
-                        <View style={{
-                            alignItems: 'center'
-                        }}>
-                            <Image source={profileImage} style={{
-                                resizeMode: 'cover',
-                                width: 80,
-                                height: 80,
-                                borderRadius: 100
-                            }} />
-                            <Text style={{
-                                paddingVertical: 5,
-                                fontWeight: 'bold',
-                            }}>
-                                {name}
-                            </Text>
-                        </View>
-                        <View style={{
-                            alignItems: 'center'
-                        }}>
-                            <Text style={{
-                                fontWeight: 'bold',
-                                fontSize: 18
-                            }}>
-                                {posts}
-                            </Text>
-                            <Text style={{
-                                fontSize: 16
-                            }}>
-                                Posts
-                            </Text>
-                        </View>
-                        <View style={{
-                            alignItems: 'center'
-                        }}>
-                            <Text style={{
-                                fontWeight: 'bold',
-                                fontSize: 18
-                            }}>
-                                {followers}
-                            </Text>
-                            <Text style={{
-                                fontSize: 16
-                            }}>
-                                Followers
-                            </Text>
-                        </View>
-                        <View style={{
-                            alignItems: 'center'
-                        }}>
-                            <Text style={{
-                                fontWeight: 'bold',
-                                fontSize: 18
-                            }}>
-                                {following}
-                            </Text>
-                            <Text style={{
-                                fontSize: 16
-                            }}>
-                                Following
-                            </Text>
-                        </View>
-
-                    </View>
+                    <View></View>
                 )
             }
+            <View style={{
+                flexDirection: 'row',
+                alignItems: 'center',
+                justifyContent: 'space-around',
+                paddingVertical: 20
+            }}>
+
+                <View style={{
+                    alignItems: 'center'
+                }}>
+                    <Image source={profileImage} style={{
+                        resizeMode: 'cover',
+                        width: 80,
+                        height: 80,
+                        borderRadius: 100
+                    }} />
+                    <Text style={{
+                        paddingVertical: 5,
+                        fontWeight: 'bold',
+                    }}>
+                        {name}
+                    </Text>
+                </View>
+                <View style={{
+                    alignItems: 'center'
+                }}>
+                    <Text style={{
+                        fontWeight: 'bold',
+                        fontSize: 18
+                    }}>
+                        {posts}
+                    </Text>
+                    <Text style={{
+                        fontSize: 16
+                    }}>
+                        Posts
+                    </Text>
+                </View>
+                <View style={{
+                    alignItems: 'center'
+                }}>
+                    <Text style={{
+                        fontWeight: 'bold',
+                        fontSize: 18
+                    }}>
+                        {followers}
+                    </Text>
+                    <Text style={{
+                        fontSize: 16
+                    }}>
+                        Followers
+                    </Text>
+                </View>
+                <View style={{
+                    alignItems: 'center'
+                }}>
+                    <Text style={{
+                        fontWeight: 'bold',
+                        fontSize: 18
+                    }}>
+                        {following}
+                    </Text>
+                    <Text style={{
+                        fontSize: 16
+                    }}>
+                        Following
+                    </Text>
+                </View>
+
+            </View>
         </View>
     );
 }
