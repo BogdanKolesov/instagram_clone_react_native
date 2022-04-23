11505b422f3234e4439a75f3f6b14b0b92d711c0
Bogdan <bogdan_kolesov@bk.ru>
2022-04-23 14:02:43 +0600

    fix:BottomTabView

--- components/screenComponents/BottomTabView.js
+++ components/screenComponents/BottomTabView.js
@@ -1,11 +1,45 @@
 import React from 'react';
-import { View, StyleSheet, Text } from 'react-native';
+import { View, StyleSheet, Text } from 'react-native'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 const BottomTabView = () => {
+    const Posts = () => {
+        return (
+            <View>
+                <Text>
+                    Post
+                </Text>
+            </View>
+        )
+    }
+
+    const Video = () => {
+        return (
+            <View>
+                <Text>
+                    Video
+                </Text>
+            </View>
+        )
+    }
+
+    const Tags = () => {
+        return (
+            <View>
+                <Text>
+                    Tags
+                </Text>
+            </View>
+        )
+    }
+
+    const Tab = createBottomTabNavigator()
     return (
-        <View>
-            <Text>BBB</Text>
-        </View>
+        <Tab.Navigator>
+            <Tab.Screen name='Posts' component={Posts} />
+            <Tab.Screen name='Video' component={Video} />
+            <Tab.Screen name='Tags' component={Tags} />
+        </Tab.Navigator>
     );
 }
 
